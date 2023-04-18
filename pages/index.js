eb350628c8a86118b98f1f2bb6c3086ac231148b
Ramirez <113019900+luciano-eth@users.noreply.github.com>
2023-04-18 15:08:45 +0000

    Commit

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,8 @@ import { useWallet } from '@solana/wallet-adapter-react';
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const TWITTER_HANDLE = '@LucianoEther';
+const TWITTER_LINK = `https://twitter.com/LucianoEther${TWITTER_HANDLE}`;
 
 const App = () => {
   const { publicKey } = useWallet();
